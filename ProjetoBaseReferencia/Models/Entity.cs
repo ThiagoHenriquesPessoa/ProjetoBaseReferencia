51629327123f04c28011b5e72a8d3255e6d06769
Thiago Henriques <thiagokxl5@gmail.com>
2022-01-30 12:13:27 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoBaseReferencia/Models/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoBaseReferencia.Models
+{
+    public class Entity
+    {
+        public Guid Id { get; set; }
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ ProjetoBaseReferencia/Models/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoBaseReferencia.Models
+{
+    public class Pessoa : Entity
+    {
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        public int Idade { get; set; }
+    }
+}
