8543b5bb9d5b413c03a0bb955ed752b06d03dbea
Thiago Henriques <thiagokxl5@gmail.com>
2022-01-30 12:45:49 -0300

    Configurando o Contexto

---
+++ ProjetoBaseReferencia/Data/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoBaseReferencia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoBaseReferencia.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        { }
+        public DbSet<Pessoa> Pessoas { get; set; }
+    }
+}
